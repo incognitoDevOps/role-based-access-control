f5ee4a42d9a5ab4b6681b605fded48b4e2107a03
incognitoDevOps <in110005620@kisiiuniversity.ac.ke>
2025-01-25 23:32:22 +0300

    frontend updates

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,43 +1,57 @@
 import Image from "next/image";
-import * as React from 'react';
+import * as React from "react";
 
-import Box from '@mui/system/Box';
-import Grid from '@mui/system/Grid';
-import styled from '@mui/system/styled';
+import Box from "@mui/system/Box";
+import Grid from "@mui/system/Grid";
+import styled from "@mui/system/styled";
 
-const Item = styled('div')(({ theme }) => ({
-  backgroundColor: '#fff',
-  border: '1px solid',
-  borderColor: '#ced7e0',
+const Item = styled("div")(({ theme }) => ({
+  backgroundColor: "#fff",
+  border: "1px solid",
+  borderColor: "#ced7e0",
   padding: theme.spacing(1),
-  borderRadius: '4px',
-  textAlign: 'center',
-  ...theme.applyStyles('dark', {
-    backgroundColor: '#1A2027',
-    borderColor: '#444d58',
+  borderRadius: "4px",
+  textAlign: "center",
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#1A2027",
+    borderColor: "#444d58",
   }),
 }));
 
 export default function Home() {
   return (
     <div>
-    <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid size={8}>
+            <Item>size=8</Item>
+          </Grid>
+          <Grid size={4}>
+            <Item>size=4</Item>
+          </Grid>
+          <Grid size={4}>
+            <Item>size=4</Item>
+          </Grid>
+          <Grid size={8}>
+            <Item>size=8</Item>
+          </Grid>
+        </Grid>
+      </Box>
+
       <Grid container spacing={2}>
-        <Grid size={8}>
-          <Item>size=8</Item>
+        <Grid size={{ xs: 6, md: 8 }}>
+          <Item>xs=6 md=8</Item>
         </Grid>
-        <Grid size={4}>
-          <Item>size=4</Item>
+        <Grid size={{ xs: 6, md: 4 }}>
+          <Item>xs=6 md=4</Item>
         </Grid>
-        <Grid size={4}>
-          <Item>size=4</Item>
+        <Grid size={{ xs: 6, md: 4 }}>
+          <Item>xs=6 md=4</Item>
         </Grid>
-        <Grid size={8}>
-          <Item>size=8</Item>
+        <Grid size={{ xs: 6, md: 8 }}>
+          <Item>xs=6 md=8</Item>
         </Grid>
       </Grid>
-    </Box>
-
     </div>
   );
 }
