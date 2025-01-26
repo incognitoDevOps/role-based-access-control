b01788bef22758317d3dea829761e7e1e39e138c
incognitoDevOps <in110005620@kisiiuniversity.ac.ke>
2025-01-26 20:41:58 +0300

    layout updated

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>Nav Component here</div>
         {children}
+        <div>Footer here</div>
       </body>
     </html>
   );
